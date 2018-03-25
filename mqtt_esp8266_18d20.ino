06a1824f0d8863afe11342799efaff84647185c1
BehindTheSciences <33276660+BehindTheSciences@users.noreply.github.com>
2018-03-25 21:57:29 +0100

    Add files via upload

---
+++ mqtt_esp8266_18d20.ino
@@ -0,0 +1,135 @@
+/*
+ ESP8266 18d20 IoT MQTT
+ Modified MQTT Example 
+
+ www.behindthesciences.com
+
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_server = "192.168.1.43";
+// Data wire is plugged into pin D1 on the ESP8266 12-E - GPIO 0
+#define ONE_WIRE_BUS 0
+
+OneWire oneWire(ONE_WIRE_BUS);
+WiFiClient espClient;
+PubSubClient client(espClient);
+DallasTemperature DS18B20(&oneWire);
+char temperatureCString[6];
+char temperatureFString[6];
+long lastMsg = 0;
+int value = 0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  DS18B20.begin();
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("BTS", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    getTemperature(); //Get Temperature value from 18D20
+    client.publish("BTS", temperatureCString);
+  }
+}
+
+
+void getTemperature() {
+  float tempC;
+  float tempF;
+  do {
+    DS18B20.requestTemperatures(); 
+    tempC = DS18B20.getTempCByIndex(0);
+    dtostrf(tempC, 2, 2, temperatureCString);
+    tempF = DS18B20.getTempFByIndex(0);
+    dtostrf(tempF, 3, 2, temperatureFString);
+    delay(100);
+  } while (tempC == 85.0 || tempC == (-127.0));
+}
